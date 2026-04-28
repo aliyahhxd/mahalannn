e885cc8d2a201858332e946222c4ae16582eda86
aliyahhxd <micaaliyahhxd@gmail.com>
2026-04-28 11:32:57 +0800

    iloveyou

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const noBtn = document.getElementById("noBtn");
-const yesBtn = document.getElementById("yesBtn");
 const card = document.getElementById("card");
 const loveScreen = document.getElementById("loveScreen");
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
 
+/* YES BUTTON */
 yesBtn.addEventListener("click", () => {
   card.style.transform = "translate(-50%, -50%) scale(0)";
   card.style.opacity = "0";
@@ -12,18 +13,16 @@ yesBtn.addEventListener("click", () => {
   }, 400);
 });
 
-noBtn.addEventListener("click", () => {
-  moveCard();
-});
-
-noBtn.addEventListener("mouseover", () => {
-  moveCard();
-});
-
-function moveCard() {
+/* NO BUTTON ESCAPES */
+function runAway() {
   const x = Math.random() * (window.innerWidth - 300);
   const y = Math.random() * (window.innerHeight - 300);
 
   card.style.left = x + "px";
   card.style.top = y + "px";
 }
+
+/* works on phone + PC */
+noBtn.addEventListener("click", runAway);
+noBtn.addEventListener("mouseover", runAway);
+noBtn.addEventListener("touchstart", runAway);
